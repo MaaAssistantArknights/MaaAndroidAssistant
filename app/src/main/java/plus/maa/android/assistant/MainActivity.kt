cde06dd96c315a6b806b1596da49a04bd9603752
dreamgyf <dreamgyf@gmail.com>
2022-10-28 19:34:39 +0800

    add createBMP function

--- app/src/main/java/plus/maa/android/assistant/MainActivity.kt
+++ app/src/main/java/plus/maa/android/assistant/MainActivity.kt
@@ -2,10 +2,13 @@ package plus.maa.android.assistant
 
 import android.content.ComponentName
 import android.content.ServiceConnection
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
 import plus.maa.android.assistant.databinding.ActivityMainBinding
+import plus.maa.android.assistant.support.createBMP
 import plus.maa.android.assistant.support.createScreenCaptureLauncher
 import plus.maa.android.assistant.support.launchScreenCapture
 
@@ -24,6 +27,13 @@ class MainActivity : AppCompatActivity() {
         val screenCaptureLauncher = createScreenCaptureLauncher(object : ServiceConnection {
             override fun onServiceConnected(name: ComponentName, service: IBinder) {
                 scsBinder = service as ScreenCaptureService.Binder
+
+                scsBinder?.captureScreen { width, height, byteArray ->
+                    val bmp = createBMP(width, height, byteArray)
+                    val bitmap = BitmapFactory.decodeByteArray(bmp, 0, bmp.size)
+
+                    Log.e("", "")
+                }
             }
 
             override fun onServiceDisconnected(name: ComponentName) {
